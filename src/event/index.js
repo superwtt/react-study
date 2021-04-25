7fbe8c48d75cf1d2a018220cdcb9c80825d7fdb0
superwtt <keepgowtt@126.com>
2021-04-25 15:18:04 +0800

    格式化

--- src/event/index.js
+++ src/event/index.js
@@ -5,36 +5,65 @@ class QrCode extends Component {
     super(props);
     this.state = {
       active: false,
+      arr: ["a", "b", "c"],
     };
+    // this.handleClick = this.handleClick.bind(this)
   }
 
   componentDidMount() {
-    document.getElementById('btn-reactandnative').addEventListener('click',e=>{
-        console.log('原生+react事件：原生事件执行')
-    })
+    document
+      .getElementById("btn-reactandnative")
+      .addEventListener("click", (e) => {
+        console.log("原生+react事件：原生事件执行");
+      });
   }
 
   componentWillUnmount() {
     document.body.removeEventListener("click");
   }
 
-  handleClick() {
-    console.log('button click');
-  }
+  //   handleClick() {
+  //     console.log(this)
+  //     console.log('button click');
+  //   }
+  handleClick = () => {
+    console.log(this);
+    console.log("button click");
+  };
 
   handleNativeAndReact(e) {
-    console.log('原生+react 事件:  当前执行react事件');
+    console.log("原生+react 事件:  当前执行react事件");
+  }
+
+  change=() =>{
+    this.setState({
+      arr: ["c", "b", "a"],
+    });
   }
 
   render() {
     return (
       <div className="pageIndex">
         <p>react event!!!</p>
-        <button id="btn-confirm" onClick={this.handleClick}>react事件</button>
-        <button id="btn-reactandnative" onClick={this.handleNativeAndReact}>原生+react</button>
+        <button id="btn-confirm" onClick={this.handleClick}>
+          react事件
+        </button>
+        <button id="btn-reactandnative" onClick={this.handleNativeAndReact}>
+          原生+react
+        </button>
+        <ul>
+          {this.state.arr.map((item, index) => {
+            return (
+              <li key={item}>
+                {item} <input type="text" />
+              </li>
+            );
+          })}
+        </ul>
+        <button onClick={this.change}>改变顺序的按钮</button>
       </div>
     );
   }
 }
 
-export default QrCode
+export default QrCode;
